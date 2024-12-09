ccf675134e4c4b952c9f35e304c889aa0ace27a8
sltz342 <133233836+sltz342@users.noreply.github.com>
2024-12-09 13:53:27 -0500

    MenuPush
    *

--- Assets/button.cs
+++ Assets/button.cs
@@ -1,13 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
-public class cameramove : MonoBehaviour
+public class button : MonoBehaviour
 {
-    
-    [SerializeField] private GameObject target;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +14,12 @@ public class cameramove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target.transform);
-        transform.Translate(Vector3.right * (Time.deltaTime * 5));
         
+    }
+    
+
+    public void OnButtonClicked()
+    {
+        SceneManager.LoadScene("MarioKartGame");
     }
 }
